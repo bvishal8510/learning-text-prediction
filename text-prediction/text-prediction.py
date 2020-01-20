f3312411145dc5c284ad142b92f342bd6ea1f125
bvishal8510 <baghel.vishal124124@gmail.com>
2020-01-20 18:19:08 +0530

    started with word prediction

--- text-prediction/text-prediction.py
+++ text-prediction/text-prediction.py
@@ -7,6 +7,7 @@ from tensorflow.python.keras.layers import LSTM
 from tensorflow.python.keras.callbacks import ModelCheckpoint
 from tensorflow.python.keras.utils import to_categorical
 import tensorflow as tf
+import utils
 
 # load ascii text and covert to lowercase
 filename = "wonderland.txt"
@@ -14,29 +15,33 @@ raw_text = open(filename).read()
 raw_text = raw_text.lower()
 # print(raw_text)
 
-# create mapping of unique chars to integers
-chars = sorted(list(set(raw_text)))
-# print(chars)
-char_to_int = dict((c, i) for i, c in enumerate(chars))
-# print(char_to_int)
+# # create mapping of unique chars to integers
+words = utils.preprocess(raw_text)
+unique_words = sorted(list(set(words)))
+
+# print(words)
+words_to_int, int_to_words = utils.create_lookup_tables(words)
+# print(words_to_int)
+# print(int_to_words)
+
 # # print(chars)
 # # print()
 # # print(char_to_int)
 
-# n_chars = len(raw_text)
-# n_vocab = len(chars)
-# # print ("Total Characters: ", n_chars) 90074
-# # print ("Total Vocab: ", n_vocab)  46
+n_words = len(words)
+n_vocab = len(unique_words)   #=chars
+print ("Total Words: ", n_words)
+print ("Total Vocab: ", n_vocab)
 
 # # prepare the dataset of input to output pairs encoded as integers
-# seq_length = 5
-# dataX = []
-# dataY = []
-# for i in range(0, n_chars - seq_length, 1):
-# 	seq_in = raw_text[i:i + seq_length]
-# 	seq_out = raw_text[i + seq_length]
-# 	dataX.append([char_to_int[char] for char in seq_in])
-# 	dataY.append(char_to_int[seq_out])
+seq_length = 5
+dataX = []
+dataY = []
+for i in range(0, n_words - seq_length, 1):
+	seq_in = raw_text[i:i + seq_length]
+	seq_out = raw_text[i + seq_length]
+	dataX.append([char_to_int[char] for char in seq_in])
+	dataY.append(char_to_int[seq_out])
 
 
 # n_patterns = len(dataX)
