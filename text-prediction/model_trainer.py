976238d3fb0b08270aba912eaf28c7f059f8eb7c
bvishal8510 <baghel.vishal124124@gmail.com>
2020-01-23 18:51:35 +0530

    done with preprocessing

--- text-prediction/model_trainer.py
+++ text-prediction/model_trainer.py
@@ -19,78 +19,68 @@ raw_text = raw_text.lower()
 # types_of_encoding = ["utf8", "cp1252"]
 
 # # create mapping of unique chars to integers
-words = utils.preprocess(raw_text) #=raw_text
-unique_words = sorted(list(set(words)))  #=chars
+words, sentences = utils.preprocess(raw_text)
+unique_words = sorted(list(set(words)))
 
 # print(words)
-word_to_int, int_to_word = utils.create_lookup_tables(words)
+word_to_int, int_to_word = utils.create_lookup_tables(unique_words)
 # print(words_to_int)
 # print(int_to_words)
 
-# # print(chars)
-# # print()
-# # print(char_to_int)
-
-n_words = len(words)  #=n_chars
 n_vocab = len(unique_words)   
-# print ("Total Words: ", n_words)
 # print ("Total Vocab: ", n_vocab)
 
 # # prepare the dataset of input to output pairs encoded as integers
-seq_length = 5
+seq_length = 3
 dataX = []
 dataY = []
-f = open("text1.txt","w")
-for i in range(0, n_words - seq_length, 1):
-	seq_in = words[i:i + seq_length]
-	seq_out = words[i + seq_length]
-	f.write(str(seq_in))
-	f.write("--->")
-	f.write(str(seq_out))
-	f.write("\n")
-	if(i==2):
-		break
-f.close()
-	# print(seq_in, "--->", seq_out)
-# 	dataX.append([word_to_int[word] for word in seq_in])
-# 	dataY.append(word_to_int[seq_out])
 
-# n_patterns = len(dataX)
-# # print ("Total Patterns: ", n_patterns)  
-# # #50 - 90024
-# # #10 - 90064
-# # #5 - 90069
-
-# # # reshape X to be [samples, time steps, features] reshape(array, shape, order)
-# X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-# # print(X)
-
-# # normalize
-# X = X / float(n_vocab)
-# # print(X)
-# # one hot encode the output variable #converts array into multiple arrays with corresponding value as 1 rest as 0
-# y = to_categorical(dataY)
-# # print(y)
-# # print(X.shape)
-# # print(y.shape)
+for sentence in sentences:
+	sentence_words = sentence.split()
+	if(len(sentence_words)>seq_length):
+		for i in range(0, len(sentence_words) - seq_length):
+			seq_in = sentence_words[i:i + seq_length]
+			seq_out = sentence_words[i + seq_length]
+			dataX.append([word_to_int[word] for word in seq_in])
+			dataY.append(word_to_int[seq_out])
 
 
-# # # define the LSTM model
-# model = Sequential()
-# model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
-# model.add(Dropout(0.2))
-# model.add(LSTM(256))
-# model.add(Dropout(0.2))
-# model.add(Dense(y.shape[1], activation='softmax'))
-# model.compile(loss='categorical_crossentropy', optimizer='adam')
+n_patterns = len(dataX)
+# print ("Total Patterns: ", n_patterns)  
+# #50 - 90024
+# #10 - 90064
+# #5 - 90069
 
-# # # define the checkpoint
-# filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
-# checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-# callbacks_list = [checkpoint]
+# # reshape X to be [samples, time steps, features] reshape(array, shape, order)
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+# print(X)
 
-# # # # to train the model only run once else regret
-# model.fit(X, y, epochs=100, batch_size=64, callbacks=callbacks_list)
+# normalize
+X = X / float(n_vocab)
+# print(X)
+# one hot encode the output variable #converts array into multiple arrays with corresponding value as 1 rest as 0
+y = to_categorical(dataY)
+# print(y)
+# print(X.shape)
+# print(y.shape)
+
+
+# # define the LSTM model
+model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(256))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# # define the checkpoint
+filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+
+# # # to train the model only run once else regret
+model.fit(X, y, epochs=300, batch_size=128, callbacks=callbacks_list)
 
 # # load the network weights
 # # filename = "weights-improvement-38-1.2788.hdf5"
--- text-prediction/utils.py
+++ text-prediction/utils.py
@@ -1,56 +1,36 @@
 import re
 from collections import Counter
 
-def preprocess(text):
+def preprocess(raw_text):
 
     # Replace punctuation with tokens so we can use them in our model
-    # text = text.lower()
-    # text = text.replace('.', ' <PERIOD> ')
-    # text = text.replace(',', ' <COMMA> ')
-    # text = text.replace('"', ' <QUOTATION_MARK> ')
-    # text = text.replace(';', ' <SEMICOLON> ')
-    # text = text.replace('!', ' <EXCLAMATION_MARK> ')
-    # text = text.replace('?', ' <QUESTION_MARK> ')
-    # text = text.replace('(', ' <LEFT_PAREN> ')
-    # text = text.replace(')', ' <RIGHT_PAREN> ')
-    # text = text.replace('--', ' <HYPHENS> ')
-    # text = text.replace('?', ' <QUESTION_MARK> ')
-    # # text = text.replace('\n', ' <NEW_LINE> ')
-    # text = text.replace(':', ' <COLON> ')
 
-    # text = text.replace('.', '')
-    unique_char = list(set(text))
+    reg = re.compile(r'\([a-z0-9 ,/]*\)')
+    matches = re.findall(reg,raw_text)
+    for match in matches:
+        raw_text = raw_text.replace(match, '')
+    reg = re.compile(r'\[[a-z0-9 ,/]*\]')
+    matches = re.findall(reg,raw_text)
+    for match in matches:
+        raw_text = raw_text.replace(match, '')
+
+    unique_char = list(set(raw_text))
     for i in unique_char:
-        if(not i.isalnum()):
-            text = text.replace(i, '')
+        if(i=='.' or i=='?' or i=='!' or i=='\n'):
+            if(i=='\n'):
+                raw_text = raw_text.replace(i, ' ')
+            else:
+                raw_text = raw_text.replace(i, '.')
+        elif((not i.isalnum()) and i!=' ' ):
+            raw_text = raw_text.replace(i, '')
+    sentences = raw_text.split('.')
+    words = raw_text.split()
 
-    words = text.split()
-    
-    # Remove all words with  5 or fewer occurences
+    # # Remove all words with  5 or fewer occurences
     word_counts = Counter(words)
     trimmed_words = [word for word in words if word_counts[word] > 5]
 
-    return trimmed_words
-
-def get_batches(int_text, batch_size, seq_length):
-    """
-    Return batches of input and target
-    :param int_text: Text with the words replaced by their ids
-    :param batch_size: The size of batch
-    :param seq_length: The length of sequence
-    :return: A list where each item is a tuple of (batch of input, batch of target).
-    """
-    n_batches = int(len(int_text) / (batch_size * seq_length))
-
-    # Drop the last few characters to make only full batches
-    xdata = np.array(int_text[: n_batches * batch_size * seq_length])
-    ydata = np.array(int_text[1: n_batches * batch_size * seq_length + 1])
-
-    x_batches = np.split(xdata.reshape(batch_size, -1), n_batches, 1)
-    y_batches = np.split(ydata.reshape(batch_size, -1), n_batches, 1)
-
-    return list(zip(x_batches, y_batches))
-
+    return trimmed_words, sentences
 
 def create_lookup_tables(words):
     """
