ac51d6882d92c108aa697b52e8500b04b928b362
bvishal8510 <baghel.vishal124124@gmail.com>
2020-05-12 16:24:59 +0530

    initial commit

--- query_maker.py
+++ query_maker.py
@@ -12,67 +12,77 @@ import codecs
 import pickle
 
 
-#reading preprocessed data
-processed_data_file = open('processed-data','rb')
+def word_predict(words):
+	#reading preprocessed data
+	processed_data_file = open('processed-data','rb')
 
-word_to_int = pickle.load(processed_data_file)
-int_to_word = pickle.load(processed_data_file)
-seq_length = pickle.load(processed_data_file)
-n_vocab = pickle.load(processed_data_file)
-len_sentences = pickle.load(processed_data_file)
-dataX = pickle.load(processed_data_file)
-dataY = pickle.load(processed_data_file)
-processed_data_file.close()
+	word_to_int = pickle.load(processed_data_file)
+	int_to_word = pickle.load(processed_data_file)
+	seq_length = pickle.load(processed_data_file)
+	n_vocab = pickle.load(processed_data_file)
+	len_sentences = pickle.load(processed_data_file)
+	dataX = pickle.load(processed_data_file)
+	dataY = pickle.load(processed_data_file)
+	processed_data_file.close()
 
-n_patterns = len(dataX)
-# print ("Total Patterns: ", n_patterns)  
+	if(len(words.lower().split()) != seq_length):
+		return ''
 
-# reshape X to be [samples, time steps, features] reshape(array, shape, order)
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-# print(X)
+	n_patterns = len(dataX)
+	# print ("Total Patterns: ", n_patterns)  
 
-# normalize            #check here dividing by len(sentences)
-X = X / float(n_vocab)
-# print(X)
+	# reshape X to be [samples, time steps, features] reshape(array, shape, order)
+	X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+	# print(X)
 
-# one hot encode the output variable #converts array into multiple arrays with corresponding value as 1 rest as 0
-# y = to_categorical(dataY)
-# print(y)
-# print(X.shape)
-# print(y.shape)
+	# normalize            #check here dividing by len(sentences)
+	X = X / float(n_vocab)
+	# print(X)
 
-model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))       #check the significance of input_shape try with X.shape[0], X.shape[1]
-model.add(Dropout(0.2))
-model.add(LSTM(256))
-model.add(Dropout(0.2))
-# model.add(Dense(y.shape[1], activation='softmax'))      #try using linear or ReLU   
-model.add(Dense(13015, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam')   #mean squared error
+	# one hot encode the output variable #converts array into multiple arrays with corresponding value as 1 rest as 0
+	# y = to_categorical(dataY)
+	# print(y)
+	# print(X.shape)
+	# print(y.shape)
 
-# load the network weights
-# filename = "weights-improvement-38-1.2788.hdf5"
-filename = "weights-improvement-298-4.2165.hdf5"
-model.load_weights(filename)
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+	model = Sequential()
+	model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))       #check the significance of input_shape try with X.shape[0], X.shape[1]
+	model.add(Dropout(0.2))
+	model.add(LSTM(256))
+	model.add(Dropout(0.2))
+	# model.add(Dense(y.shape[1], activation='softmax'))      #try using linear or ReLU   
+	model.add(Dense(13015, activation='softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='adam')   #mean squared error
+
+	# load the network weights
+	# filename = "weights-improvement-38-1.2788.hdf5"
+	filename = "weights-improvement-298-4.2165.hdf5"
+	model.load_weights(filename)
+	model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 
-print("Enter starting ",seq_length," words:")
-starting_words = input().lower().split()
-# print(starting_words)
-pattern = [word_to_int[word] for word in starting_words]
-# print("pattern",pattern)
+	# print("Enter starting ",seq_length," words:")
+	starting_words = words.lower().split()
+	# print(starting_words)
+	pattern = [word_to_int[word] for word in starting_words]
+	# print("pattern",pattern)
 
-# generate characters
-for i in range(1):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
-	x = x / float(n_vocab)
-	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
-	result = int_to_word[index]
-	sys.stdout.write(result)
-	sys.stdout.write(' ')
-	pattern.append(index)
-	pattern = pattern[1:len(pattern)]
+	# generate characters
+	for i in range(1):
+		x = numpy.reshape(pattern, (1, len(pattern), 1))
+		x = x / float(n_vocab)
+		prediction = model.predict(x, verbose=0)
+		index = numpy.argmax(prediction)
+		result = int_to_word[index]
+		pattern.append(index)
+		pattern = pattern[1:len(pattern)]
+	# print()
+	# print("Inside script :", result)
+	# print()
+	# sys.stdout.write(result)
+	return result
 
-print()
+
+words = sys.argv
+# print(words)
+word_predict(sys.argv[1])
